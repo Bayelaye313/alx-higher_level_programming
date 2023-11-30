eef9d5034330021f3bd16df07ba1cd5020a065fd
Bayelaye313 <bayelaye0510@gmail.com>
2023-11-30 21:19:27 +0000

    final

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python3
-import sys
-from calculation_1 import add, sub, div, mul
+if __name__ == "__main__":
+    import sys
 
-def print_usage_and_exit():
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+    nargs = len(sys.argv) - 1
+    if nargs != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-def print_unknown_operator_and_exit():
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
+    op = sys.argv[2]
+    if op != '+' and op != '-' and op != '*' and op != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-if __name__ == '__main__':
-    i = len(sys.argv) - 1
-    if i != 3:
-        print_usage_and_exit()
+    from calculator_1 import add, sub, mul, div
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
 
-    a, ops, b = map(int, sys.argv[1:])
-    
-    if ops == '+':
-        result = add(a, b)
-    elif ops == '-':
-        result = sub(a, b)
-    elif ops == '*':
-        result = mul(a, b)
-    elif ops == '/':
-        if b == 0:
-            print("Error: Division by zero")
-            sys.exit(1)
-        result = div(a, b)
+    if op == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
     else:
-        print_unknown_operator_and_exit()
-
-    print("{} {} {} = {}".format(a, ops, b, result))
+        print("{} / {} = {}".format(a, b, div(a, b)))
