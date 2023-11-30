af3674575dd8cd9c078781898d12c1e76ab6636b
Bayelaye313 <bayelaye0510@gmail.com>
2023-11-30 17:51:37 +0000

    sysargv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    i = len(sys.argv) - 1
+	args = sys.argv[1:]
+
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
+
+        for i, arg in enumerate(args, start = 1):
+			print("{}: {}".format(i, arg))
