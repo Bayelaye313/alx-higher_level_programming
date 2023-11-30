50951166de112da348f6683ea12d4dee5b6661b1
Bayelaye313 <bayelaye0510@gmail.com>
2023-11-30 21:15:01 +0000

    clc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -14,21 +14,21 @@ if __name__ == '__main__':
     i = len(sys.argv) - 1
     if i != 3:
         print_usage_and_exit()
-        
+
     a, ops, b = map(int, sys.argv[1:])
-    if ops not in {'+', '-', '*', '/'}:
-        print_unknown_operator_and_exit()
     
-    result = None
     if ops == '+':
         result = add(a, b)
     elif ops == '-':
         result = sub(a, b)
     elif ops == '*':
         result = mul(a, b)
-    else:
+    elif ops == '/':
         if b == 0:
             print("Error: Division by zero")
             sys.exit(1)
         result = div(a, b)
+    else:
+        print_unknown_operator_and_exit()
+
     print("{} {} {} = {}".format(a, ops, b, result))
