9423daf51a901dfb7d20a4778e0257f87650336a
bayelaye313 <bayelaye0510@gmail.com>
2024-02-14 18:18:36 +0000

    fixing syntaxcat 9-full_creation.sql | mysql -hlocalhost -uroot -p hbtn_0c_0

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,10 @@
---create a snd table
-CREATE TABLE IF NOT EXISTS `second_table`(`id` INT, `name` VARCHAR(256), `score` INT);
-INSERT INTO `second_table`(id, name, score) VALUES(1, "John", 10);
-INSERT INTO `second_table`(id, name, score) VALUES(2, "Alex", 3);
-INSERT INTO `second_table`(id, name, score) VALUES(3, "Bob", 14);
-INSERT INTO `second_table`(id, name, score) VALUES(4, "George", 8);
+-- create a second_table and insert values
+CREATE TABLE IF NOT EXISTS `second_table` (
+    `id` INT,
+    `name` VARCHAR(256),
+    `score` INT
+);
+INSERT INTO `second_table` (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO `second_table` (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO `second_table` (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO `second_table` (id, name, score) VALUES (4, 'George', 8);
