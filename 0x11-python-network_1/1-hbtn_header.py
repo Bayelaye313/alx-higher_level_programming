a3082d0b23575d5a065c4059a4925be5a61bad4e
Bayelaye313 <bayelaye0510@gmail.com>
2024-07-05 22:50:33 +0000

    send a request

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        print(dict(response.headers).get("X-Request-Id"))
