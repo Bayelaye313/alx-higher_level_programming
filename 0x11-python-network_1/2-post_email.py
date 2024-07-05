78ce77df4ca260684708d81380b2c39be8ac7ba6
Bayelaye313 <bayelaye0510@gmail.com>
2024-07-05 23:30:07 +0000

    basics

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
-"""Displays the body of the response of a POST request to a given URL with an email.
+"""Sends a POST request to a given URL with a given email.
 
 Usage: ./2-post_email.py <URL> <email>
 """
 import sys
-import urllib.request
 import urllib.parse
+import urllib.request
+
 
 if __name__ == "__main__":
-    # Récupération des arguments de la ligne de commande
     url = sys.argv[1]
     email = sys.argv[2]
-    
-    # Préparation des données à envoyer dans la requête POST
-    val = {'email': email}
+    val = {"email": email}
     data = urllib.parse.urlencode(val).encode("ascii")
-    
-    # Création de la requête POST
-    req = urllib.request.Request(url, data=data)
-    
-    # Envoi de la requête et récupération de la réponse
+
+    req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
-        # Lecture et affichage du corps de la réponse décodé en UTF-8
-        print(response.read().decode('utf-8'))
+        print(response.read().decode("utf-8"))
